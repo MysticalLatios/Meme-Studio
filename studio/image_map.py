096ff18bac99649cee3fe60f3f34505bac0789ca
JoshuaAlpert <jma858@humboldt.edu>
2018-10-30 16:44:56 -0700

    Start of image class stuff, added basic ppm suport

---
+++ studio/image_map.py
@@ -0,0 +1,51 @@
+#Its how we store our images
+
+
+
+class pixel:
+    red_value = 0
+    green_value = 0
+    blue_value = 0
+    max_rgb = 255; #This is hardcoded for now TODO:add it as required input in the pixel constructor
+
+    def set_red(self, value):
+        self.red_value = value
+
+    def set_green(self, value):
+        self.green_value = value
+
+    def set_blue(self, value):
+        self.blue_value = value
+
+
+class ppm:
+    ppm_spec = 0
+    dimension_x = 0
+    dimension_y = 0
+    max_rgb = 0
+
+    #Dictionary
+    #First key is xpos
+    #Second key is ypos
+    #Second is a pixel
+    image_map = {{}}
+
+
+    def set_spec(self, spec):
+        self.ppm_spec = spec
+
+    def set_x(self, x):
+        self.dimension_x = x
+
+    def set_y(self, y):
+        self.dimension_y = y
+
+    def set_max_rgb(self, rgb):
+        self.max_rgb = rgb
+
+    def add_to_map(self, x, y, pix):
+        self.image_map.add(x,{y:pix})
+
+
+
+
