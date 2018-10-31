6e65361fa8fbe9d99c5eb47a979cf3a5806b32c9
JoshuaAlpert <jma858@humboldt.edu>
2018-10-30 20:56:54 -0700

    added a filter

---
+++ studio/filters.py
@@ -0,0 +1,16 @@
+import image_map
+import random
+
+#lets make it more blue
+def more_blue(img_in: image_map.ppm):
+    ''' Makes an image more blue '''
+
+    x = img_in.get_x()
+    y = img_in.get_y()
+
+    for i in range(0, y):
+        for j in range(0, x):
+            pix = img_in.get_pixel(j,i)
+            pix.set_blue(int(pix.get_blue()) + random.randint(-25, 25) % 255)
+            pix.set_green(int(pix.get_green()) + random.randint(-25, 25) % 255)
+            pix.set_red(int(pix.get_red()) + random.randint(-25, 25) % 255)
